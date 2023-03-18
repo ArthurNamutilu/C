970484ff3d9fcc0c8b8cc6c3c41e886459807733
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-03-19 00:37:08 +0300

    added hello file

--- hello.c
+++ hello.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    char name [] ="GgRicky";
-    printf("Hello %s, welcome\n", name); 
+/**
+  * main - Begining of code
+  *
+  * Return: 0 Success
+  */
+
+int main(void)
+{
+	char name[] = "GgRicky";
+
+	printf("Hello %s, welcome\n", name);
+	return (0);
 }
